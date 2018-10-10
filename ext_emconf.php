80546e311d247e06db025917519b1d653193ac9a
Malte Riechmann <kontakt@malteriechmann.de>
2018-10-10 06:57:35 +0200

    [TASK] Add support for TYPO3 v9 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'visuellverstehen',
     'state' => 'beta',
     'clearCacheOnLoad' => false,
-    'version' => '0.1.2',
+    'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
         ]
     ]
 ];

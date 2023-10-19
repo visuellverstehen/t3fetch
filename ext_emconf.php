478a896719cde20a45d3f792333b95bbe708dc4f
Rune Piper <kontakt@runepiper.de>
2023-10-19 12:58:34 +0200

    chore: allow install in projects with TYPO3 12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-11.5.99',
+            'typo3' => '8.7.0-12.4.99',
         ]
     ]
 ];
